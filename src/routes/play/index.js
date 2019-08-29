8b0739626b98f49e168bf7ca7875970cf7dd4a2a
Ioannis Karavas <io.karavas@gmail.com>
2019-08-29 14:48:54 +0000

    Chord identification , parsing of JSON

--- src/routes/play/index.js
+++ src/routes/play/index.js
@@ -1,14 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import ChordButton from '../../components/play/chordbutton'
 import {fetchSongIfNeeded} from '../../actions'
 import {useSelector, useDispatch} from 'react-redux'
-import {parseJSONChords} from '../../utils/index.js'
+import {parseJSONChords, identifyChord} from '../../utils/index.js'
+import YouTube from 'react-youtube'
+
 
 export const Play = (props) => {
     const songName = props.match.params.songName
     const song = useSelector(state => state.song)
     const isLoading = useSelector(state => state.isLoadingSong)
     const dispatch = useDispatch()
+
+    const [currentChord, setChord] = useState('')
+
 
     useEffect(()=>{
             dispatch(fetchSongIfNeeded(songName))
@@ -20,11 +25,42 @@ export const Play = (props) => {
 
     const chordsTimeline = parseJSONChords(song.chords)
 
+    console.log(chordsTimeline)
+
+    const opts = {
+        height: '390',
+        width: '640',
+        playerVars: {
+            autoplay: 1
+        }
+    }
+
+    const handleYTState = (event)=>{
+        const currentTime = event.target.getCurrentTime()
+
+        if (event.data == 1) {
+            console.log('started playing')
+
+            setChord(identifyChord(currentTime, chordsTimeline).name)
+
+            // setChord(event.target.getCurrentTime())
+        }
+    }
+
     return (
         <div className="section-title col-lg-8 col-md-10 ml-auto mr-auto text-center">
             <h3>Play song {songName}</h3>
 
             <div className="row">
+
+                <YouTube
+                    videoId={song.link}
+                    opts={opts}
+                    onStateChange={handleYTState}
+                />
+
+                {currentChord}
+
                 {/*<ChordButton {...song}/>*/}
 
             </div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,6 @@
 export const parseJSONChords = (song) => {
     let chords = []
+    let rangedChords = []
 
     if (typeof(song)==='string') {
         song = JSON.parse(song)
@@ -10,8 +11,8 @@ export const parseJSONChords = (song) => {
         song[group].chords.map((chord)=>{
             chord.time.map((step)=>{
                 let chordObj = {
-                    time: step.step*1000,
-                    chord: chord.chord
+                    time: step.step,
+                    name: chord.chord
                 }
                 chords.push(chordObj)
             })
@@ -22,5 +23,31 @@ export const parseJSONChords = (song) => {
         return chordA.time - chordB.time
     })
 
-    return chords
+    let lastChord = chords[0]
+
+    chords.map((chord,index)=>{
+
+        if (chord.name !== lastChord.name) {
+            let chordObj = {
+                name : lastChord.name,
+                from : parseFloat( (index!==0)?lastChord.time:0 ),
+                to   : chord.time-0.1
+            }
+            rangedChords.push(chordObj)
+            lastChord = chord
+        }
+
+    })
+
+    return rangedChords
+}
+
+export const identifyChord = (time, rangedChords) => {
+    for (let chord of rangedChords) {
+        if ((chord.from <= time) && ( chord.to >= time )) {
+            return chord
+            break
+        }
+    }
+    return {from:0,to:0,name:''} // default
 }
